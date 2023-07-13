480a7fd62e12bfad2c150b5a62f2ca393ab9a55a
ajg1029 <urban.seonbi.deon@gmail.com>
2023-07-13 14:21:05 +0900

    create test001 component, install firebase

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test001 />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test001 from './components/Test001.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Test001
   }
 }
 </script>
---
+++ src/components/Test001.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>Test001</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test001',
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
